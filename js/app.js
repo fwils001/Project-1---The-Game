57cf72e61c6cf41ae378f4be67fe3c330415ccd1
fwils001 <fwils001@ucr.edu>
2021-08-12 19:43:31 -0700

    what is happening?

--- js/app.js
+++ js/app.js
@@ -6,19 +6,22 @@ class Hero {
      }
 }
 const game = {
-    newHero: null,
+    newHero: "",
 
-    start: function(name) {
+    start(name) {
         const inputname=document.getElementById('yourname')
         const nameDisplay=inputname.value
+        console.log(nameDisplay)
         this.newHero=new Hero(name);
     },
 
-    pageOne: function() {
+    pageOne() {
         //remove what's on the page (function remove everything but the background image. Select them and remove them.)
-        const words = document.getElementById("words")
+        const words = document.querySelector("#words")
+        const list = document.querySelector("#list")
         words.style.display="none"
-    }
+    },
+
 }
 const endGame = ()=>{
     alert("GAME OVER!")
@@ -46,46 +49,55 @@ const turnPage = ()=>{
 }
     
 //EVENT LISTENERS
-let form = document.getElementById('nameform')
-    form.addEventListener('click', (event) =>{
-let submitName = document.getElementById('yourname').value;
-console.log(document.querySelector('#name'))
-document.querySelector('#name').innerHTML = `${submitName}`
-    document.querySelector('#name').style.display = "none"
-//Add the value from submit name to the game object
-game.name=document.getElementById("yourname").value
-    game.start(submitName)
-    form.style.display = "none"
+let form = document.querySelector('.form')
+    form.addEventListener('submit', (event) =>{
+        event.preventDefault()
+        const userInput = document.querySelector("#yourname")
+        console.log(userInput.value)
+        document.querySelector("#header").style.display = "none"
+        document.querySelector("#words").style.display = "none"
+        document.querySelector("#list").style.display = "none"
+        document.querySelector('#name').style.display = "none"
+    //    let submitName = document.getElementById('yourname').value;
+    //     console.log(document.querySelector('#name'))
+    //     document.querySelector('#name').innerHTML = `${submitName}`
+        
+    //     //Add the value from submit name to the game object
+    //     game.name=document.getElementById("yourname").value
+    //     game.start(submitName)
+    //     game.pageOne(submitName)
+    //     form.style.display = "none"
     })
 
 
 
-   /* let enterWorld = confirm("Do you want to begin your journey?")
-    console.log(enterWorld)
-if(enterWorld == true) {
-   let firstChoice = prompt("You come across a fork in the road. Which way do you go? Mt. Doom/Jungle of Death?")
 
-   if (firstChoice == "Mt. Doom") {
-       alert("Now entering Mt. Doom")
-       turnPage();
-       
-       let doomChoice = prompt("Do you want to climb the mountain alone or use a tour guide? alone/guide")
-       
-       if (doomChoice == "alone") {
-           alert("You get stuck just as it is getting dark. You freeze to death. Remember: always ask for help when you need it.")
-            endGame();
 
-       }else if(doomChoice == "guide") {
-            alert ("Your tour guide is eaten by a bear! Luckily, you are able to find your way back to the lodge.")
-       }
-   }
-   else if(firstChoice == "Jungle of Death") {
-       alert("Now entering the Jungle of Death");
-   }
+//   let enterWorld = confirm("Do you want to begin your journey?")
+//     console.log(enterWorld)
+// if(enterWorld == true) {
+//    let firstChoice = prompt("You come across a fork in the road. Which way do you go? Mt. Doom/Jungle of Death?")
+
+//    if (firstChoice == "Mt. Doom") {
+//        alert("Now entering Mt. Doom")
+//        turnPage();
+       
+//        let doomChoice = prompt("Do you want to climb the mountain alone or use a tour guide? alone/guide")
+       
+//        if (doomChoice == "alone") {
+//            alert("You get stuck just as it is getting dark. You freeze to death. Remember: always ask for help when you need it.")
+//             endGame();
+
+//        }else if(doomChoice == "guide") {
+//             alert ("Your tour guide is eaten by a bear! Luckily, you are able to find your way back to the lodge.")
+//        }
+//    }
+//    else if(firstChoice == "Jungle of Death") {
+//        alert("Now entering the Jungle of Death");
+//    }
     
-}
+// }
 
-else {
-    alert("You decided to end life before it even began. Good bye.")
-}
-*/
+// else {
+//     alert("You decided to end life before it even began. Good bye.")
+// }
